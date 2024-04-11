d6c182574ce9f306954da5a877c421d42b602aff
Sahan Udara <sahanherath444@gmail.com>
2024-04-11 16:56:30 +0530

    QA Fixes

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
+      <View st>
+
+      </View>
+
     </View>
   );
 }
